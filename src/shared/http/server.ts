afd936d2ae7a38e840cafee613066f5cdf399592
Democrito <demohenry6@gmail.com>
2021-03-17 21:58:04 -0300

    Tratamento de errors com classe AppError

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,29 @@
-console.log("hello typescript")
+import express, { NextFunction, Request, response, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError){
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+});
+
+app.listen(3333, () => {
+  console.log('API Started');
+});
