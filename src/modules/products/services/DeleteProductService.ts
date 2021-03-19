dd072f7ae055e2446c38cb7c0b73ec1244391c82
Democrito <demohenry6@gmail.com>
2021-03-19 15:58:54 -0300

    Criação do DeleteProductService

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,23 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from './../typeorm/repositories/ProductsRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  id: string;
+}
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    //pesquisa se há produto com id informado
+    const product = await productsRepository.findOne(id);
+    //senão houver dispara erro
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    await productsRepository.remove(product);
+  }
+}
+
+export default DeleteProductService;
