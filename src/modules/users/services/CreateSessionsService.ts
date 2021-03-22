ee930e98710dd0ecead03b267cd2c1cf71bd3daf
Democrito <demohenry6@gmail.com>
2021-03-21 21:55:43 -0300

    inst JWT> criação de token em CreateSessions

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,5 +1,6 @@
 import AppError from '@shared/errors/AppError';
 import { compare, hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -9,12 +10,13 @@ interface IRequest {
   password: string;
 }
 
-/* interface IResponse {
+interface IResponse {
   user: User,
-} */
+  token: string,
+}
 
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -28,8 +30,15 @@ class CreateSessionsService {
       throw new AppError('Incorret email/password combination.', 401);
     }
 
+    const token = sign({}, 'e393a4cb95c53b735be086c11e5e926d', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
 
-    return user;
+    return {
+      user,
+      token,
+    };
   }
 }
 
